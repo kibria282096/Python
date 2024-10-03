6008168e234e4d9948dbd034a4de24684df12a57
kibria282096 <jucse282096@gmail.com>
2024-10-03 19:34:48 +0600

    conditional operator

---
+++ condition.py
@@ -0,0 +1,20 @@
+'''
+is_hot= False
+is_cold = False
+
+if is_hot:
+    print("its a hot day")
+    print("Drink plenty of water")
+elif is_cold:
+    print("Cold day")
+      
+else:
+    print("Enjoy")
+print("lovely day")
+'''
+house_price = 1000
+buyers_credit_more = True 
+if buyers_credit_more :
+    print(f"10%_discount: ${0.1*house_price}")#formatted string
+else:
+    print("Unable to buy")
