e5432595d58743f05a7e3419a59391729012b6b3
kibria282096 <jucse282096@gmail.com>
2024-10-03 18:41:38 +0600

    string method

---
+++ abc.py
@@ -0,0 +1,7 @@
+course = "python for beginners"
+print(course.find('beginners'))
+print(course.replace('beginners','absolute beginners'))
+print(course.upper())
+print('Python' in course)
+print(len(course))
+print(course.title())
