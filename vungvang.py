239f015362931a7c30b093ec89a1a6a2c5f4519d
kibria282096 <jucse282096@gmail.com>
2024-10-03 20:59:59 +0600

    logical operator

---
+++ vungvang.py
@@ -0,0 +1,10 @@
+has_high_income = True
+has_good_credit = True
+has_criminal_record= True
+
+if has_good_credit and not has_criminal_record:
+    print("Eligible for loan")
+else:
+    print("Not Eligible")
+'''if has_high_income and has_good_credit:
+    print("Eligible")'''
