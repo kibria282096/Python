92279fb323ad1fdcadc3f9fbd49d56489e63f330
kibria282096 <jucse282096@gmail.com>
2024-10-03 21:12:06 +0600

    name and temparature

---
+++ thermo.py
@@ -0,0 +1,18 @@
+'''temparature = 30
+
+if temparature>30:
+    print("Its a hot day")
+else: 
+    print("Its not hot day")'''
+
+name = "Rafi"
+
+if len(name)<3:
+    print("Name mustbe greater")
+elif len(name)>50:
+    print("Name must be a short")
+else:
+    print(f"perfect {name}")    
+        
+
+    
