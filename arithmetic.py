32283b928741a415e2bcb72792e1240ff3df1a24
kibria282096 <jucse282096@gmail.com>
2024-10-03 19:02:41 +0600

    arithmetic

---
+++ arithmetic.py
@@ -0,0 +1,19 @@
+print(10/3)
+print(10%3)
+print("power: "+str(10**3))
+print("power: ",10**3)
+x = 10
+x = x+3
+print(x)
+
+x -=3
+print(x)
+
+x= 10+3*2**2
+print(x)
+'''
+operator precedence:
+    exponentiation 2**3
+    multiplication or division
+    addition or subtraction
+'''    
